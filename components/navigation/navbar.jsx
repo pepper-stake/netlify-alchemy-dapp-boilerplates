57f5c717570e8a66d161d98eb42c7558e96b4a09
pepper-stake <131142765+pepper-stake@users.noreply.github.com>
2024-07-13 21:09:36 +0200

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+			<a href="https://pepperstake.fi" target={"_blank"}>
 				<img className={styles.alchemy_logo} src="/redpepper.png"></img>
 			</a>
 			<ConnectButton></ConnectButton>
